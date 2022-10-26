2bc16c3373b5bbfaccb2d899f8def8dd9d1c868a
DimaKrupenko <dimakrupenko77@gmail.com>
2022-10-26 21:56:19 +0300

    первое задание готово

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,30 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
+const ref = {
+  start: document.querySelector('[data-start]'),
+  stop: document.querySelector('[data-stop]'),
+  body: document.querySelector('body'),
+};
+
+ref.start.addEventListener('click', onStart);
+ref.stop.addEventListener('click', onStop);
+
+let startId;
+let isActive = false;
+function onStart(evt) {
+  if (isActive) {
+    return;
+  }
+  isActive = true;
+
+  startId = setInterval(() => {
+    ref.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+}
+
+function onStop() {
+  clearInterval(startId);
+  isActive = false;
+}
