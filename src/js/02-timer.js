9ceeca1e3f7f5cdcfe200c117a367d1e8f9b11c9
DimaKrupenko <dimakrupenko77@gmail.com>
2022-10-29 18:04:23 +0300

    выполнил на половину 2 задание

---
+++ src/js/02-timer.js
@@ -0,0 +1,33 @@
+import 'flatpickr/dist/flatpickr.min.css';
+import flatpickr from 'flatpickr';
+
+const ref = {
+  input: document.querySelector('#datetime-picker'),
+  button: document.querySelector('[data-start]'),
+  days: document.querySelector('[data-days]'),
+};
+
+// ref.input.addEventListener('close', () => {
+//   options.onClose();
+// });
+
+document.getElementById('start').disabled = true;
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    if (selectedDates[0] < new Date()) {
+      document.getElementById('start').disabled = true;
+      return window.alert('Please choose a date in the future');
+    }
+    document.getElementById('start').disabled = false;
+    console.log(selectedDates[0]);
+    console.log(new Date());
+    ref.days.textContent = selectedDates[0] - new Date();
+  },
+};
+
+const calendar = flatpickr('#datetime-picker', options);
