d1925f31807d708445172ea39a60aee806deab61
DimaKrupenko <dimakrupenko77@gmail.com>
2022-10-30 23:16:47 +0200

    сделал второе задание

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,5 +1,6 @@
 import 'flatpickr/dist/flatpickr.min.css';
 import flatpickr from 'flatpickr';
+import Notiflix from 'notiflix';
 
 const ref = {
   input: document.querySelector('#datetime-picker'),
@@ -24,7 +25,7 @@ const options = {
   onClose(selectedDates) {
     if (selectedDates[0] < new Date()) {
       document.getElementById('start').disabled = true;
-      return window.alert('Please choose a date in the future');
+      return Notiflix.Notify.warning('Please choose a date in the future');
     }
     document.getElementById('start').disabled = false;
     dataValue = selectedDates[0] - new Date();
@@ -34,13 +35,38 @@ const options = {
 
 const calendar = flatpickr('#datetime-picker', options);
 
-ref.button.addEventListener('click', onStart);
+ref.button.addEventListener('click', onStartId);
 
-function onStart() {
-  ref.days.textContent = 0 + [convertMs(dataValue).days];
-  ref.hours.textContent = convertMs(dataValue).hours;
-  ref.minutes.textContent = convertMs(dataValue).minutes;
-  ref.seconds.textContent = convertMs(dataValue).seconds;
+function onStartId() {
+  const conversion = convertMs(dataValue);
+
+  ref.days.textContent = conversion.days;
+  ref.hours.textContent = conversion.hours;
+  ref.minutes.textContent = conversion.minutes;
+  ref.seconds.textContent = conversion.seconds;
+
+  document.getElementById('start').disabled = true;
+
+  const startTime = Date.now();
+
+  if (dataValue == 0) {
+    clearTimeout(onStartId);
+  }
+  setInterval(() => {
+    const currentTime = Date.now();
+    const deltaTime = currentTime - startTime;
+    const timeComponents = convertMs(deltaTime);
+
+    ref.days.textContent = pad(conversion.days - timeComponents.days);
+    ref.hours.textContent = pad(conversion.hours - timeComponents.hours);
+    ref.minutes.textContent = pad(conversion.minutes - timeComponents.minutes);
+    ref.seconds.textContent = pad(conversion.seconds - timeComponents.seconds);
+    // console.log(timeComponents);
+  }, 1000);
+}
+
+function pad(value) {
+  return String(value).padStart(2, '0');
 }
 
 function convertMs(ms) {
